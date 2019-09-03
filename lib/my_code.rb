8a006dd0e6e97715a6587dea7ed28bbfd7cb133e
Alexis Rangel <alexiscash97@gmail.com>
2019-09-03 05:09:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -23,8 +23,8 @@ end
 
 def reduce_to_total(array, start=0)
   total = 0 
-  for i in start..array.length do 
-    puts i
+  for i in start...array.length do 
+    puts array[i]
   end
   total
 end
@@ -32,7 +32,7 @@ end
 
 
 
-puts reduce_to_total([1,2,3,4])
+puts reduce_to_total([5,7,16,28])
 
 
 

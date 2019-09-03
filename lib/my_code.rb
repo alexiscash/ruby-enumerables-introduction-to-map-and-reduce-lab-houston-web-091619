f2c1b6f46fa8c0b0f71ab96964955c8a47c93b5c
Alexis Rangel <alexiscash97@gmail.com>
2019-09-03 04:55:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,12 +1,21 @@
 # My Code here....
 def map_to_negativize(array)
   new_array = []
-  array.each { |elem| new_array << elem *= -1 }
+  array.each { |elem| new_array << elem * -1 }
   new_array
 end
 
 def map_to_no_change(array)
   array
 end
+
+def map_to_double(array)
+  new_array =[]
+  array.each { |elem| new_array << elem * 2
+
+
+
+
+
 
 puts map_to_negativize([1,2,3,4])

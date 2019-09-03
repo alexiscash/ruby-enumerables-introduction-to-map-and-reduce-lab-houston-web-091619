2abf32d11b0386e6ba66d1206d8a892f444cda32
Alexis Rangel <alexiscash97@gmail.com>
2019-09-03 05:04:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,7 +15,19 @@ def map_to_double(array)
   new_array
 end
 
+def map_to_square(array)
+  new_array = []
+  array.each { |elem| new_array << elem ** 2 }
+  new_array
+end
 
+def reduce_to_total(array, start=0)
+  total = 0 
+  start..array.length do 
+    |elem| total += elem 
+  end
+  total
+end
 
 
 
